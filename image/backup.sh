e2e9288edad7e8124ffe5d0a06af4388c7906fb0
Jose Antonio Gonzalez Prada <josgonza@redhat.com>
2019-04-10 11:49:45 +0200

    Refactoring: - Getting `uid` and `gid` in a more efficient way. - Source folder check - Remove PVC destination folder.

--- image/backup.sh
+++ image/backup.sh
@@ -17,27 +17,20 @@ is_empty() {
   return "${empty}"
 }
 
-check_folder() {
-  local folder="${1}"
-  local exists=0
-
-  if [[ ! -d "${folder}" ]]; then
-    exists=1
-  fi
-
-  return "${exists}"
-}
-
-getid(){
+get_restrictive_id(){
   local fs="${1}"
-  local type="${2}"
   local id=""
 
-  if [[ "${type}" == "user" ]]; then
-    id="$( find ${fs} -maxdepth 1 -type f ! -perm -g+r ! -perm -o+r ! -path '*/\.*' -exec stat -c '%u' {} \; | awk 'FNR == 1' )"
-  else
-    id="$( find ${fs} -maxdepth 1 -type f ! -perm -g+r ! -perm -o+r ! -path '*/\.*' -exec stat -c '%g' {} \; | awk 'FNR == 1' )"
-  fi
+  id="$( find ${fs} -maxdepth 1 -type f ! -perm -g+r ! -perm -o+r ! -path '*/\.*' -exec stat -c '%u:%g' {} \; | awk 'FNR == 1' )"
+
+  echo "${id}"
+}
+
+get_id(){
+  local fs="${1}"
+  local id=""
+
+  id="$( find ${fs} -maxdepth 1 -type f -exec stat -c '%u:%g' {} \; | awk 'FNR == 1' )"
 
   echo "${id}"
 }
@@ -46,25 +39,30 @@ main () {
   local -r src="/source"
   local -r bck_dir="${BCK_FOLDER}"
   local -r dst="/backup"
-  local -r pvc="${PVC_FOLDER}"
   local -r user_name="rsyncuser"
 
   local src_dir="${src}/"
 
-  if ! is_empty "${bck_dir}" && check_folder "${src}/${bck_dir}" ; then
+  if ! is_empty "${bck_dir}" && [[ -d "${src}/${bck_dir}" ]] ; then
     src_dir="${src}/${bck_dir}/"
   fi
 
-  dst_dir="${dst}/${pvc}/"
+  dst_dir="${dst}/"
 
-  user_uid="$( getid ${src_dir} user )"
+  user_ugid="$( get_restrictive_id ${src_dir} )"
+ 
+  if [[ "${user_ugid}" == "" ]]; then
+    user_ugid="$( get_id ${src_dir} )"
 
-  if [[ "${user_uid}" == "" ]]; then
-    echo "The file system/folder ${src_dir} is empty" 
-    exit "${E_EMPTYFS}"
+    if [[ "${user_ugid}" == "" ]]; then
+      echo "The file system/folder ${src_dir} is empty" 
+      exit "${E_EMPTYFS}"
+    fi
+
   fi
 
-  user_gid="$( getid ${src_dir} group )"
+  user_uid=$(echo $user_ugid | cut -d':' -f1)
+  user_gid=$(echo $user_ugid | cut -d':' -f2)
 
   sed -i "s/${user_name}:x:10002:10002/${user_name}:x:${user_uid}:${user_uid}/g" /etc/passwd
   sed -i "s/${user_name}:x:10002/${user_name}:x:${user_gid}/g" /etc/group
